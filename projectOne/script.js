37726aca97b9f86f22826f8d2b9c20bc34682fcf
mohdafnaan <mohdafnaan833@gmail.com>
2026-01-15 09:16:28 +0530

    dom projects completed

--- projectOne/script.js
+++ projectOne/script.js
@@ -13,7 +13,6 @@ addFriend.addEventListener("click",()=>{
     addFriend.style.backgroundColor = "grey"
     statuss.style.color = "green"
     friend = 1;
-    // img.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5CNpbCZwUTqWxdSEWknRJwDcy9o4LzcyXkA&s") 
 }
     else{
     statuss.innerHTML = "Stranger"
@@ -21,6 +20,5 @@ addFriend.addEventListener("click",()=>{
     friend = 0
     addFriend.innerHTML = "Add Friend"
     addFriend.style.backgroundColor = "cadetblue"
-    // img.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAr7IjHOfBjEQMFYPt8ZsBh2iD2WqTw5gmEQhhULtetdRcr-8NSEn32PBOt_1DgH1FV4kH9k3GR7-AsfPoNbHf1BW6iIHTQS3wrdNeEkUk&s=10")
     }
 })
